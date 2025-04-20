9750cc1489b83a1b8f92d9a1bd6dab3521f5f2e6
Caio Henrique <crobatmobile@gmail.com>
2025-04-20 11:14:12 -0300

    adição de login de usuário

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.model';
 import { UserService } from './users.service';
+import { UserController } from './users.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    controllers: [],
     providers: [UserService],
     exports: [UserService],
 })
